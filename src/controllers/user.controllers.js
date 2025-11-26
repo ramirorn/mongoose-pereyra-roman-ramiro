0ae7acbef994296b6188dc4369a314c608954a0b
Ramiro <ramiroroman306@gmail.com>
2025-11-26 20:07:24 -0300

    Controladores de user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,14 +1,17 @@
 import { UserModel } from "../models/user.model.js";
+import { PostModel } from "../models/post.model.js";
+import { ProfilePictureModel } from "../models/profile.picture.model.js";
 
 // Creacion de un nuevo usuario
 export const createNewUser = async (req, res) => {
-  const { username, email, password, person } = req.body;
+  const { username, email, password, person, profile_picture } = req.body;
   try {
-    const user = await UserModel.create({ username, email, password, person });
+    const user = await UserModel.create({ username, email, password, person, profile_picture });
+    const populatedUser = await UserModel.findById(user._id).populate('profile_picture');
     res.status(201).json({
       ok: true,
       msg: "Usuario creado con exito",
-      data: user,
+      data: populatedUser,
     });
   } catch (err) {
     return res.status(500).json({
@@ -21,7 +24,7 @@ export const createNewUser = async (req, res) => {
 // Traer todos los usuarios
 export const getAllUser = async (req, res) => {
   try {
-    const users = await UserModel.find();
+    const users = await UserModel.find().populate('profile_picture');
     res.status(200).json({
       ok: true,
       data: users,
@@ -38,7 +41,7 @@ export const getAllUser = async (req, res) => {
 export const getUserById = async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await UserModel.findById(id);
+    const user = await UserModel.findById(id).populate('profile_picture');
     res.status(200).json({
       ok: true,
       data: user,
@@ -54,7 +57,7 @@ export const getUserById = async (req, res) => {
 // Actualizar un usuario
 export const updateUser = async (req, res) => {
   const { id } = req.params;
-  const { username, email, password, person } = req.body;
+  const { username, email, password, person, profile_picture } = req.body;
   try {
     const updated = await UserModel.findByIdAndUpdate(
       id,
@@ -63,9 +66,10 @@ export const updateUser = async (req, res) => {
         email,
         password,
         person,
+        profile_picture,
       },
       { new: true }
-    );
+    ).populate('profile_picture');
     res.status(200).json({
       ok: true,
       msg: "Usuario actualizado correctamente",
@@ -83,14 +87,28 @@ export const updateUser = async (req, res) => {
 export const deleteUser = async (req, res) => {
   const { id } = req.params;
   try {
+    // Eliminacion logica del usuario
     const deleted = await UserModel.findByIdAndUpdate(
       id,
       { isActive: false },
       { new: true }
     );
+
+    // Eliminacion en cascada: desactivar posts del usuario
+    await PostModel.updateMany(
+      { author: id },
+      { exist: false }
+    );
+
+    // Eliminacion en cascada: desactivar profile picture del usuario
+    await ProfilePictureModel.updateMany(
+      { user: id },
+      { exist: false }
+    );
+
     return res.status(200).json({
       ok: true,
-      msg: "Usuario borrado correctamente",
+      msg: "Usuario borrado correctamente (eliminacion logica y en cascada aplicada)",
     });
   } catch (err) {
     return res.status(500).json({
